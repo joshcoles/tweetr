c6414a0218379b557694a1436d7fb6fd9e42fdc6
Josh Coles <josh@joshcoles.com>
2016-11-18 03:40:58 +0000

    Replaced local DB with MongoDB. App is functional but needs to be refactored.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@
 
 // Basic express setup:
 
+
+//new dependencies=======================================
+const {MongoClient} = require("mongodb");
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+//=======================================================
+
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
@@ -14,10 +20,32 @@ app.use(express.static("public"));
 
 
 //replace this with a connection to mongo 'db'
-const db = require("./lib/in-memory-db");
+// const db = require("./lib/in-memory-db");
 
 
 // 1. connect to mongo, until you get a mongo "db"
+MongoClient.connect(MONGODB_URI, (err, db) => {
+    if (err) {
+      console.log(`Failed to connect: ${MONGODB_URI}`);
+      throw err;
+    }
+
+  const DataHelpers = require("./lib/data-helpers.js")(db);
+
+  // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
+  // so it can define routes that use it to interact with the data layer.
+  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+
+  // Mount the tweets routes at the "/tweets" path prefix:
+  app.use("/tweets", tweetsRoutes);
+
+  app.listen(PORT, () => {
+    console.log("Example app listening on port " + PORT);
+  });
+
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+});
+
 
 
 // The `data-helpers` module provides an interface to the database of tweets.
@@ -27,15 +55,3 @@ const db = require("./lib/in-memory-db");
 //
 // Because it exports a function that expects the `db` as a parameter, we can
 // require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
-
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
-
-// Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
-
-app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
-});
